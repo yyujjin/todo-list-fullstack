80126509f1a14817a32460eec94b3fff9a08d4b6
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-06 02:18:19 +0900

    ✨ TodoList 기능 구현: 할 일 추가, 목록 표시, 완료 버튼 추가

--- src/App.js
+++ src/App.js
@@ -6,27 +6,98 @@ function App() {
   return (
     <>
       <h1>To-Do List</h1>
+
       <div id="todo-container">
-        <input
-          type="text"
-          id="todo-input"
-          placeholder="할 일을 입력하세요"
-        ></input>
-        <button id="add-btn">할 일 추가</button>
-        <ul id="todo-list"></ul>
+        <AddTodo />
+        <ul id="todo-list">
+          <List />
+        </ul>
       </div>
     </>
   );
 }
 
+function AddTodo() {
+  const [todo, setTodo] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        id="todo-input"
+        placeholder="할 일을 입력하세요"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      ></input>
+      <AddButton todo={todo} setTodo={setTodo} />
+    </>
+  );
+}
+
+function AddButton({ todo, setTodo }) {
+  const sendTodo = () => {
+    fetch("http://localhost:8080/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: todo }),
+    })
+      .then((res) => res.json())
+
+      .catch((error) => console.error("에러 발생:", error));
+    setTodo("");
+  };
+
+  return (
+    <button id="add-btn" onClick={sendTodo}>
+      할 일 추가
+    </button>
+  );
+}
+
 function List() {
   const [todos, setTodos] = useState([]);
+  console.log(todos);
   useEffect(() => {
     fetch("http://localhost:8080/todos")
       .then((response) => response.json())
       .then((json) => setTodos(json));
   }, []);
-  return todos.map((todo) => <li key={todo.id}>{todo.title}</li>);
+  return todos.map((todo) => (
+    <li key={todo.id}>
+      {todo.completed ? <s>{todo.title}</s> : todo.title}
+      <CompletedButton
+        id={todo.id}
+        todo={todo}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    </li>
+  ));
+}
+
+function CompletedButton({ id, todo, todos, setTodos }) {
+  const updateState = () => {
+    fetch(`http://localhost:8080/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed: !todo.completed }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setTodos(
+          todos.map((t) =>
+            t.id === json.id ? { ...t, completed: json.completed } : t
+          )
+        );
+      })
+      .catch((error) => console.error("에러 발생:", error));
+  };
+
+  return <button onClick={updateState}>완료</button>;
 }
 
 export default App;
