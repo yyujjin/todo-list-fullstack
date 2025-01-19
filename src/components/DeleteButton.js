e0658087eba6b34398afd256afec195d298e41b6
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-19 16:24:57 +0900

    ♻️ fetch->axios 삭제 요청 방식 변경

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,12 +1,19 @@
+import axios from "axios";
+
 export default function DeleteButton({ id, todos, setTodos }) {
   const deleteTodo = () => {
-    fetch(`http://localhost:8080/todos/${id}`, {
-      method: "DELETE",
-    })
-      .then(() => {
+    axios
+      .delete(`http://localhost:8080/todos/${id}`)
+      .then(function (response) {
         setTodos(todos.filter((todo) => todo.id !== id));
       })
-      .catch((error) => console.error("에러 발생:", error));
+      .catch(function (error) {
+        // 에러 핸들링
+        console.log(error);
+      })
+      .finally(function () {
+        // 항상 실행되는 영역
+      });
   };
 
   return <button onClick={deleteTodo}>삭제</button>;
