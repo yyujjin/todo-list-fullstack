53266143357a02a9aa0fa5c8554d46a50feeee9d
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-19 16:15:04 +0900

    ♻️ fetch->axios 방식으로 patch 방식 변경

--- src/components/CompletedButton.js
+++ src/components/CompletedButton.js
@@ -1,21 +1,23 @@
+import axios from "axios";
+
 export default function CompletedButton({ id, todo, todos, setTodos }) {
   const updateState = () => {
-    fetch(`http://localhost:8080/todos/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ completed: !todo.completed }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
+    axios
+      .patch(`http://localhost:8080/todos/${id}`, {
+        completed: !todo.completed,
+      })
+      .then(function (response) {
         setTodos(
           todos.map((t) =>
-            t.id === json.id ? { ...t, completed: json.completed } : t
+            t.id === response.data.id
+              ? { ...t, completed: response.data.completed }
+              : t
           )
         );
       })
-      .catch((error) => console.error("에러 발생:", error));
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return <button onClick={updateState}>완료</button>;
