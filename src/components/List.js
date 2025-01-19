4a925da868552c9c61eb543481a98fe96b25c4ca
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-19 15:38:30 +0900

    ♻️ HTTP 요청 방식 fetch에서 axios로 변경하여 GET 요청 수정

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,28 @@
 import CompletedButton from "./CompletedButton";
 import DeleteButton from "./DeleteButton";
 import { useEffect } from "react";
+import axios from "axios";
 
 export default function List({ todos = { todos }, setTodos = { setTodos } }) {
-  console.log(todos);
   useEffect(() => {
-    fetch("http://localhost:8080/todos")
-      .then((response) => response.json())
-      .then((json) => setTodos(json));
+    axios
+      .get("http://localhost:8080/todos")
+      .then(function (response) {
+        setTodos(response.data);
+      })
+      .catch(function (error) {
+        // 에러 핸들링
+        console.log(error);
+      })
+      .finally(function () {
+        // 항상 실행되는 영역
+      });
   }, []);
+
   if (todos.length === 0) {
     return <p style={{ backgroundColor: "white" }}>"할 일을 추가하세요!</p>;
   }
+
   return todos.map((todo) => (
     <li key={todo.id}>
       {todo.completed ? <s>{todo.title}</s> : todo.title}
